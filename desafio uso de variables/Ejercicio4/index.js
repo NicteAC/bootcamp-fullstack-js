6954727451048ab92818dc87b6833ca2f58cbf74
NicteAC <carola.aliaga@gmail.com>
2022-05-24 13:37:35 -0400

    ok

--- desafio uso de variables/Ejercicio4/index.js
+++ desafio uso de variables/Ejercicio4/index.js
@@ -1,8 +1,10 @@
 var diasUsuario = prompt("Ingrese cantidad de dias");
 
-var anio = (365 / parseInt(diasUsuario)).toFixed();
-var sobranteAnio = parseInt(diasUsuario) % 365;
-var semanas = sobranteAnio / 7;
-var sobranteSemanas =
-console.log(anio);
-console.log(semanas)
+var anio = Math.floor(parseInt(diasUsuario) / 365);
+var semanas = Math.floor((parseInt(diasUsuario) % 365) / 7);
+var dias = Math.floor((parseInt(diasUsuario) % 365) % 7);
+alert(`El resultado es el siguiente : 
+Años ${anio}
+Semanas ${semanas}
+Días ${dias}
+`);
