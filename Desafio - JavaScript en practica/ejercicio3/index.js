a923be6ad7172fa585739b03b717336e5f5fcc13
Carolina <carolina@carpetres.cl>
2022-05-28 09:58:16 -0400

    desafio js en practica finalizado

--- Desafio - JavaScript en practica/ejercicio3/index.js
+++ Desafio - JavaScript en practica/ejercicio3/index.js
@@ -1,2 +1,24 @@
-let numeroUno = document.getElementById("valor1");
-let numeroDos = document.getElementById("valor2");
+
+let botonSumar = document.getElementById("btn-sumar");
+let botonRestar = document.getElementById("btn-restar");
+
+botonSumar.addEventListener("click", () => {
+  let numeroUno = document.getElementById("valor1");
+  let numeroDos = document.getElementById("valor2");
+  numeroUno = parseInt(numeroUno.value);
+  numeroDos = parseInt(numeroDos.value);
+  document.querySelector(".resultado").innerHTML = numeroUno + numeroDos;
+});
+
+botonRestar.addEventListener("click", () => {
+  let numeroUno = document.getElementById("valor1");
+  let numeroDos = document.getElementById("valor2");
+  numeroUno = parseInt(numeroUno.value);
+  numeroDos = parseInt(numeroDos.value);
+  let resultadoNegativo = numeroUno - numeroDos;
+  if(resultadoNegativo <= 0){ 
+    document.querySelector(".resultado").innerHTML = 0
+  }else{
+    document.querySelector(".resultado").innerHTML = numeroUno - numeroDos;
+  }
+});
