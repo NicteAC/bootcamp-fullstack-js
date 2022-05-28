83864436d439ebdd15e518463f8025c95e7e036a
Carolina <carolina@carpetres.cl>
2022-05-27 21:02:31 -0400

    desafio cachipun ok

--- Desafio-cachipun/index.js
+++ Desafio-cachipun/index.js
@@ -1,19 +1,37 @@
-$(document).ready(function () {
-  $(".my__modal").modal("show");
-});
-var jugadaUsuario = Swal.fire({
-  title: `Ingresa tu jugada:
-    Piedra 👊= 0
-    Papel 🖐= 1
-    Tijeras ✌= 2`,
-  input: "text",
-});
-var numeroJugadas = Swal.fire({
-  title: "Ingresa la cantidad de veces que deseas jugar",
-  input: "text",
-});
-
-console.log(jugadaUsuario);
+var cantidadJugadas = prompt(`Ingrese la cantidad de veces que desea jugar
+Maximo de 5 jugadas`);
 var piedra = 0;
 var papel = 1;
 var tijeras = 2;
+function juego(jugador, maquina) {
+  if (!jugador) return;
+  if (jugador === maquina) {
+    console.log("empate");
+  } else if (
+    (jugador === 1 && maquina === 0) ||
+    (jugador === 2 && maquina === 1) ||
+    (jugador === 0 && maquina === 2)
+  ) {
+    console.log("ganaste");
+  } else {
+    console.log("perdiste");
+  }
+}
+while (cantidadJugadas > 0 && cantidadJugadas <= 5) {
+  var cachipun = [0, 1, 2];
+  var jugadaUsuario = prompt(
+    `Ingresa tu jugada:
+      Piedra 👊= 0
+      Papel 🖐= 1
+      Tijeras ✌= 2`
+  );
+  var maquina = Math.floor(Math.random() * (0, cachipun.length - 1));
+  console.log(
+    `Has elegido ${jugadaUsuario} y la maquina ha elegido ${maquina}`
+  );
+  cantidadJugadas = cantidadJugadas - 1;
+  juego(jugadaUsuario, maquina);
+}
+if (cantidadJugadas > 5) {
+  alert("El maximo de juegos es 5");
+}
---
+++ desafio arrays y objetos - parte1/index.js
@@ -0,0 +1,140 @@
+let radiologia = [
+  {
+    hora: "11:00:00 AM",
+    especialista: "IGNACIO SCHULZ",
+    paciente: "FRANCISCA ROJAS",
+    rut: "9878782-1",
+    prevision: "FONASA",
+  },
+  {
+    hora: "11:30:00 AM",
+    especialista: "FEDERICO SUBERCASEAUX",
+    paciente: "PAMELA ESTRADA",
+    rut: "15345241-3",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "03:00:00 PM",
+    especialista: "FERNANDO WURTHZ",
+    paciente: "ARMANDO LUNA",
+    rut: "16445345-9",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "03:30:00 PM",
+    especialista: "ANA MARIA GODOY",
+    paciente: "MANUEL GODOY",
+    rut: "17666419-0",
+    prevision: "FONASA",
+  },
+  {
+    hora: "04:00:00 PM",
+    especialista: "PATRICIA SUAZO",
+    paciente: "RAMON ULLOA",
+    rut: "14989389-K",
+    prevision: "FONASA",
+  },
+];
+let traumatologia = [
+  {
+    hora: "08:00:00 AM",
+    especialista: "MARIA PAZ ALTUZARRA",
+    paciente: "PAULA SANCHEZ",
+    rut: "15554774-5",
+    prevision: "FONASA",
+  },
+  {
+    hora: "10:00:00 AM",
+    especialista: "RAUL ARAYA",
+    paciente: "ANGELICA NAVAS",
+    rut: "15444147-9",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "10:30:00 AM",
+    especialista: "MARIA ARRIAGADA",
+    paciente: "ANA KLAPP",
+    rut: "17879423-9",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:00:00 AM",
+    especialista: "ALEJANDRO BADILLA",
+    paciente: "FELIPE MARDONES",
+    rut: "1547423-6",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:30:00 AM",
+    especialista: "CECILIA BUDNIK",
+    paciente: "DIEGO MARRE",
+    rut: "16554741-K",
+    prevision: "FONASA",
+  },
+  {
+    hora: "12:00:00 PM",
+    especialista: "ARTURO CAVAGNARO",
+    paciente: "CECILIA MENDEZ",
+    rut: "9747535-8",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "12:30:00 PM",
+    especialista: "ANDRES KANACRI",
+    paciente: "MARCIAL SUAZO",
+    rut: "11254785-5",
+    prevision: "ISAPRE",
+  },
+];
+let dental = [
+  {
+    hora: "08:30:00 AM",
+    especialista: "ANDREA ZUÑIGA",
+    paciente: "MARCELA RETAMAL",
+    rut: "11123425-6",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:00:00 AM",
+    especialista: "MARIA PIA ZAÑARTU",
+    paciente: "ANGEL MUÑOZ",
+    rut: "9878789-2",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:30:00 AM",
+    especialista: "SCARLETT WITTING",
+    paciente: "MARIO KAST",
+    rut: "7998789-5",
+    prevision: "FONASA",
+  },
+  {
+    hora: "01:00:00 PM",
+    especialista: "FRANCISCO VON TEUBER",
+    paciente: "KARIN FERNANDEZ",
+    rut: "18887662-K",
+    prevision: "FONASA",
+  },
+  {
+    hora: "01:30:00 PM",
+    especialista: "EDUARDO VIÑUELA",
+    paciente: "HUGO SANCHEZ",
+    rut: "17665461-4",
+    prevision: "FONASA",
+  },
+  {
+    hora: "02:00:00 PM",
+    especialista: "RAQUEL VILLASECA",
+    paciente: "ANA SEPULVEDA",
+    rut: "14441281-0",
+    prevision: "ISAPRE",
+  },
+];
+document.write(`
+<p>Primera atención: ${radiologia[0].paciente} - ${radiologia[0].prevision} | Última atención: ${radiologia[4].paciente} - ${radiologia[4].prevision}</p>
+<p>Primera atención: ${traumatologia[0].paciente} - ${traumatologia[0].prevision} | Última atención: ${traumatologia[6].paciente} - ${traumatologia[6].prevision}</p>
+<p>Primera atención: ${dental[0].paciente} - ${dental[0].prevision} | Última atención: ${dental[5].paciente} - ${dental[5].prevision}</p>`);
+
+console.table(radiologia);
+console.table(traumatologia);
+console.table(dental);
