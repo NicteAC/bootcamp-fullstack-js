2b5f876c3f000426015c5649056b5ebdf41f967e
NicteAC <carola.aliaga@gmail.com>
2022-07-26 19:51:31 -0400

    Desafío - Creando y analizando nuestro propio blog

--- BBDD/Desafio - Creando y analizando nuestro propio blog.sql
+++ BBDD/Desafio - Creando y analizando nuestro propio blog.sql
@@ -21,18 +21,40 @@ create table comentarios(
     FOREIGN KEY(post_id) REFERENCES posts(id)
 );
 --3. Insertar los siguientes registros. (1 Punto)
-select * from usuarios;
-copy usuarios from 'C:\Users\carol\Desktop\fullstack\BBDD\usuarios.csv' delimiter ',' csv header;
-select * from posts;
-copy posts from 'C:\Users\carol\Desktop\fullstack\BBDD\posts.csv' delimiter ',' csv header;
-select * from comentarios;
-copy comentarios from 'C:\Users\carol\Desktop\fullstack\BBDD\comentarios.csv' delimiter ',' csv header;
+select *
+from usuarios;
+copy usuarios
+from 'C:\Users\carol\Desktop\fullstack\BBDD\usuarios.csv' delimiter ',' csv header;
+select *
+from posts;
+copy posts
+from 'C:\Users\carol\Desktop\fullstack\BBDD\posts.csv' delimiter ',' csv header;
+select *
+from comentarios;
+copy comentarios
+from 'C:\Users\carol\Desktop\fullstack\BBDD\comentarios.csv' delimiter ',' csv header;
 --4. Seleccionar el correo, id y título de todos los post publicados por el usuario 5. (1 Punto)
-select email, id, titulo 
+select *
+from usuarios
+    left join posts on usuarios.id = posts.usuario_id
+where usuario_id = 5;
 --5. Listar el correo, id y el detalle de todos los comentarios que no hayan sido realizados por el usuario con email usuario06@hotmail.com. (1 Punto)
-
+SELECT *
+from usuarios
+    left join comentarios on usuarios.id = comentarios.usuario_id
+where usuarios.email != 'usuario06@hotmail.com';
 --6. Listar los usuarios que no han publicado ningún post. (1 Punto)
-
+select *
+from usuarios
+    left join posts on usuarios.id = posts.usuario_id
+where titulo is null;
 --7. Listar todos los post con sus comentarios (incluyendo aquellos que no poseen comentarios). (1 Punto)
-
+select *
+from posts
+    full outer join comentarios on posts.id = comentarios.post_id;
 --8. Listar todos los usuarios que hayan publicado un post en Junio. (2 Puntos)
+select usuarios.id,
+    email
+FROM usuarios
+    LEFT JOIN posts on usuarios.id = posts.usuario_id
+WHERE fecha between '2020-06-01' and '2020-06-30';
